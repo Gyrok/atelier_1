8e51ae43c0ef637c6b65572d97f0f04fb471da36
Jerome_P <jerome.petrucci@cpe.fr>
2021-12-15 10:10:24 +0100

    request useform

--- React/src/components/UserForm/UserForm.js
+++ React/src/components/UserForm/UserForm.js
@@ -1,37 +1,50 @@
-import React from 'react';
+import React, {useState } from 'react';
 import { Form, Header,Button } from 'semantic-ui-react'
 
-function processInput(){
-
-}
-
-function readJson () {
-    console.log(this)
-    
-    fetch('http://localhost:3000/Card',{
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json',
-            }
-         } )
-    .then((response) => response.json()).then(json => {
-        console.log(json);
-    }).catch(function () {
-        console.log("error");
-    })
- }
 
 
-function submitOrder(){
+function submitOrder(Login,surname,lastname,password,pwdCheck){
     console.log("hello")
-    readJson();
+
+    if(password == pwdCheck){
+        fetch('http://localhost:8082/user',{
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body : JSON.stringify({
+            login:Login,
+            surName:surname,
+            lastName: lastname,
+            pwd:password,
+            img:"'https://www.nicepng.com/png/full/982-9820051_heart-2352306885-deadpool-png.png'",
+            money:"5000",})
+            } )
+        .then(res => {
+            console.log(res);
+            console.log("Wellcome")
+            window.alert("Wellcome new player!")
+        }).catch(function () {
+            console.log("error");
+        })
+    }
+    else {
+        window.alert("Confirm your password")
+    }
 }
+
 function cancelOrder(){
     window.location.reload();
 }
 
 export const UserForm=(props)=>{
+    const [login, setLogin] = useState("");
+    const [surname, setSurname] = useState("");
+    const [lastname, setLastName] = useState("");
+    const [pwd, setPwd] = useState("");
+    const [pwdCheck, setPwdCheck] = useState("");
+    
     return(
         
         <Form>
@@ -39,21 +52,24 @@ export const UserForm=(props)=>{
             User Registration
         </Header>
         <Form.Field>
-            <Form.Input type="text" label="Surname" placeholder="surname" onChange={processInput}  name="surname"/>
+            <Form.Input type="text" label="Login" placeholder="login" onChange={e => setLogin(e.target.value)}  name="login"/>
         </Form.Field>
         <Form.Field>
-            <Form.Input type="text" label="Name" placeholder="name" onChange={processInput}  name="name"/>
+            <Form.Input type="text" label="Surname" placeholder="surname" onChange={e => setSurname(e.target.value)}  name="surname"/>
+        </Form.Field>
+        <Form.Field>
+            <Form.Input type="text" label="Lastame" placeholder="lastname" onChange={e => setLastName(e.target.value)}  name="lastname"/>
         </Form.Field>
 
         <Form.Field>
-            <Form.Input type="password" label="Pwd" placeholder="password" onChange={processInput}  name="pwd"/>
+            <Form.Input type="password" label="Pwd" placeholder="password" onChange={e => setPwd(e.target.value)}  name="pwd"/>
         </Form.Field>
         <Form.Field>
-            <Form.Input type="password" label="Pwd re-enter" placeholder="password" onChange={processInput}  name="pwdCheck"/>
+            <Form.Input type="password" label="Pwd re-enter" placeholder="password" onChange={e => setPwdCheck(e.target.value)}  name="pwdCheck"/>
         </Form.Field>
 
         <Button type='submit' onClick={()=>cancelOrder()}>Cancel</Button>
-        <Button type='submit' onClick={()=>submitOrder()}>Submit</Button>
+        <Button type='submit' onClick={()=>submitOrder(login,surname,lastname,pwd,pwdCheck)}>Submit</Button>
     </Form>
 
     )
