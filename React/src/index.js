497a103283a17d273ab25ae28f9ade2cb1e84d53
Gyrok <45562652+Gyrok@users.noreply.github.com>
2021-12-19 10:04:03 +0100

    moved stuff

--- React/src/index.js
+++ React/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 // import the created Main component
 import {Main} from './Main';
 //add Semantic UI import
-import 'semantic-ui-css/semantic.min.css'
+import 'semantic-ui-css'
 
 //Insert a <Main> component inside the <div id='root'/>
 // send the property title to the App component
